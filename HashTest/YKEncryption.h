24914f7f8d1d257c2c7fa07bba5d8f108107819b
ksnowlv <ksnowlv@163.com>
2018-04-17 18:09:54 +0800

    first commit

---
+++ HashTest/YKEncryption.h
@@ -0,0 +1,13 @@
+//
+//  YKEncryption.h
+//  HashTest
+//
+//  Created by ksnowlv on 2018/4/17.
+//  Copyright © 2018年 ksnowlv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YKEncryption : NSObject
+
+@end
---
+++ HashTest/YKEncryption.m
@@ -0,0 +1,13 @@
+//
+//  YKEncryption.m
+//  HashTest
+//
+//  Created by ksnowlv on 2018/4/17.
+//  Copyright © 2018年 ksnowlv. All rights reserved.
+//
+
+#import "YKEncryption.h"
+
+@implementation YKEncryption
+
+@end
